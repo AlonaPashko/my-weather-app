a498235888cbaeb792c286edcb7dfb564f3ea946
AlonaPashko <105510952+AlonaPashko@users.noreply.github.com>
2022-06-27 15:43:26 +0200

    Add weather icon
    add a default icon and add an icon from OpenWeather

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
-  function displayCurrentTime(){
+  function formatDate(timestamp){
     let date = new Date();
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     
-    let displayTime = document.querySelector("#current-time");
-    
     let day = days[date.getDay()];
     let hours = date.getHours();
+    
     if (hours < 10){
       hours = `0${hours}`;
     }
@@ -14,14 +13,16 @@
     if(minutes < 10){
       minutes = `0${minutes}`;
     }
-    displayTime.innerHTML = `${day}, ${hours} : ${minutes}`;
+    return `${day}, ${hours} : ${minutes}`;
   }
-function searchCity(city)
+
+  function searchCity(city)
 {
   let apiKey = "0872dacd74a30528eb08cd3ce78d89ee";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeatherCondition);
 }
+
 function searchLocation(position)
 {
   let apiKey = "0872dacd74a30528eb08cd3ce78d89ee";
@@ -37,20 +38,23 @@ function handleSubmit(event)
   let city = document.querySelector("#input-form").value;
   searchCity(city);
 }
+
 function showWeatherCondition(response){
   document.querySelector("#current-city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#description").innerHTML = response.data.weather[0].main;
+  document.querySelector("#current-time").innerHTML = formatDate(response.data.dt * 1000);
+  document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
 }
+
 function getCurrentLocation(event){
 event.preventDefault();
 navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-displayCurrentTime();
-  
   let userSearch = document.querySelector("#search-form");
   userSearch.addEventListener("submit", handleSubmit);
 
